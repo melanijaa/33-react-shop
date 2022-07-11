afd78fd0760ef55a4561e5e97eff87b532cbc41d
Melanija <melanija.sibakovska1@gmail.com>
2022-07-11 14:28:01 +0300

    front

--- src/Components/Front/Front.jsx
+++ src/Components/Front/Front.jsx
@@ -1,6 +1,39 @@
+import Nav from "./Nav";
+import List from "./List";
+import { useState, useEffect } from "react";
+import { authConfig } from "../../Functions/auth";
+import axios from "axios";
+import FrontContext from "./FrontContext";
+import SortFilter from "./SortFilter";
+
 function Front() {
-    return (
-        <h1>FRONT</h1>
-    )
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3003/products", authConfig())
+      .then((res) => setProducts(res.data.map((p, i) => ({ ...p, row: i }))));
+  }, []);
+
+  return (
+    <FrontContext.Provider
+      value={{
+        products,
+        setProducts,
+      }}
+    >
+      <Nav />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <SortFilter />
+          </div>
+          <div className="col-12">
+            <List />
+          </div>
+        </div>
+      </div>
+    </FrontContext.Provider>
+  );
 }
 export default Front;
