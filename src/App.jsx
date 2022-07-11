0fb299c73e7f4afe467b777ca118c36aa138e3cf
Melanija <melanija.sibakovska1@gmail.com>
2022-07-11 16:13:21 +0300

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,294 +1,120 @@
-const express = require("express");
-const app = express();
-const port = 3003;
-const cors = require("cors");
-app.use(express.json({ limit: "10mb" }));
-app.use(cors());
-const mysql = require("mysql");
-const md5 = require("js-md5");
-const uuid = require("uuid");
+import "./bootstrap.css";
+import "./App.scss";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
+import Back from "./Components/Back/Back";
+import Front from "./Components/Front/Front";
+import { login, logout, authConfig } from "./Functions/auth";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(express.json());
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RequireAuth role="user">
+              <Front />
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/logout" element={<LogoutPage />} />
+        <Route
+          path="/admin"
+          element={
+            <RequireAuth role="admin">
+              <Back show="admin" />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/admin/cats"
+          element={
+            <RequireAuth role="admin">
+              <Back show="cats" />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/admin/products"
+          element={
+            <RequireAuth role="admin">
+              <Back show="products" />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "la_ma_shop",
-});
+function RequireAuth({ children, role }) {
+  const [view, setView] = useState(<h2>Please wait...</h2>);
 
-const doAuth = function (req, res, next) {
-  if (0 === req.url.indexOf("/admin")) {
-    // admin
-    const sql = `
-        SELECT
-        name, role
-        FROM users
-        WHERE session = ?
-    `;
-    con.query(sql, [req.headers["authorization"] || ""], (err, results) => {
-      if (err) throw err;
-      if (!results.length || results[0].role !== "admin") {
-        res.status(401).send({});
-        req.connection.destroy();
-      } else {
-        next();
+  useEffect(() => {
+    axios
+      .get("http://localhost:3003/login-check?role=" + role, authConfig())
+      .then((res) => {
+        if ("ok" === res.data.msg) {
+          setView(children);
+        } else {
+          setView(<Navigate to="/login" replace />);
+        }
+      });
+  }, [children]);
+
+  return view;
+}
+
+function LoginPage() {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+
+  const doLogin = () => {
+    axios.post("http://localhost:3003/login", { user, pass }).then((res) => {
+      console.log(res.data);
+      if ("ok" === res.data.msg) {
+        login(res.data.key);
+        navigate("/", { replace: true });
       }
     });
-  } else if (
-    0 === req.url.indexOf("/login-check") ||
-    0 === req.url.indexOf("/login")
-  ) {
-    next();
-  } else {
-    // fron
-    const sql = `
-        SELECT
-        name, role
-        FROM users
-        WHERE session = ?
-    `;
-    con.query(sql, [req.headers["authorization"] || ""], (err, results) => {
-      if (err) throw err;
-      if (!results.length) {
-        res.status(401).send({});
-        req.connection.destroy();
-      } else {
-        next();
-      }
-    });
-  }
-};
-app.use(doAuth);
-
-// AUTH
-app.get("/login-check", (req, res) => {
-  let sql;
-  let requests;
-  if (req.query.role === "admin") {
-    sql = `
-        SELECT
-        name
-        FROM users
-        WHERE session = ? AND role = ?
-        `;
-    requests = [req.headers["authorization"] || "", req.query.role];
-  } else {
-    sql = `
-        SELECT
-        name
-        FROM users
-        WHERE session = ?
-        `;
-    requests = [req.headers["authorization"] || ""];
-  }
-  con.query(sql, requests, (err, result) => {
-    if (err) throw err;
-    if (!result.length) {
-      res.send({ msg: "error" });
-    } else {
-      res.send({ msg: "ok" });
-    }
-  });
-});
-
-app.post("/login", (req, res) => {
-  const key = uuid.v4();
-  const sql = `
-    UPDATE users
-    SET session = ?
-    WHERE name = ? AND pass = ?
-  `;
-  con.query(sql, [key, req.body.user, md5(req.body.pass)], (err, result) => {
-    if (err) throw err;
-    if (!result.affectedRows) {
-      res.send({ msg: "error", key: "" });
-    } else {
-      res.send({ msg: "ok", key });
-    }
-  });
-});
-
-// CATS
-app.post("/admin/cats", (req, res) => {
-  const sql = `
-    INSERT INTO cats
-    (title)
-    VALUES (?)
-    `;
-  con.query(sql, [req.body.title], (err, result) => {
-    if (err) throw err;
-    res.send({
-      result,
-      msg: { text: "OK, new Cat was created", type: "success" },
-    });
-  });
-});
-
-app.get("/admin/cats", (req, res) => {
-  const sql = `
-  SELECT *
-  FROM cats
-  ORDER BY title
-`;
-  con.query(sql, (err, result) => {
-    if (err) throw err;
-    res.send(result);
-  });
-});
-
-app.delete("/admin/cats/:id", (req, res) => {
-  const sql = `
-    DELETE FROM cats
-    WHERE id = ?
-    `;
-  con.query(sql, [req.params.id], (err, result) => {
-    if (err) throw err;
-    res.send({ result, msg: { text: "OK, Cat gone", type: "success" } });
-  });
-});
-
-app.put("/admin/cats/:id", (req, res) => {
-  const sql = `
-    UPDATE cats
-    SET title = ?
-    WHERE id = ?
-    `;
-  con.query(sql, [req.body.title, req.params.id], (err, result) => {
-    if (err) throw err;
-    res.send({
-      result,
-      msg: { text: "OK, Cat updated. Now it is as new", type: "success" },
-    });
-  });
-});
-
-// Products
-app.post("/admin/products", (req, res) => {
-  const sql = `
-    INSERT INTO products
-    (title, price, in_stock, cats_id, photo)
-    VALUES (?, ?, ?, ?, ?)
-    `;
-  con.query(
-    sql,
-    [
-      req.body.title,
-      req.body.price,
-      req.body.inStock,
-      req.body.cat,
-      req.body.photo,
-    ],
-    (err, result) => {
-      if (err) throw err;
-      res.send({
-        result,
-        msg: { text: "OK, new and shiny product was created", type: "success" },
-      });
-    }
+  };
+  return (
+    <div>
+      <div>
+        name:{" "}
+        <input
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        password:{" "}
+        <input
+          type="password"
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+        ></input>
+      </div>
+      <button onClick={doLogin}>Login</button>
+    </div>
   );
-});
+}
 
-app.get("/admin/products", (req, res) => {
-  const sql = `
-  SELECT p.id, price, p.title, c.title AS cat, in_stock, last_update AS lu, photo
-  FROM products AS p
-  LEFT JOIN cats AS c
-  ON c.id = p.cats_id
-  ORDER BY title
-`;
-  con.query(sql, (err, result) => {
-    if (err) throw err;
-    res.send(result);
-  });
-});
+function LogoutPage() {
+  useEffect(() => logout(), []);
+  return <Navigate to="/login" replace />;
+}
 
-app.delete("/admin/products/:id", (req, res) => {
-  const sql = `
-    DELETE FROM products
-    WHERE id = ?
-    `;
-  con.query(sql, [req.params.id], (err, result) => {
-    if (err) throw err;
-    res.send({ result, msg: { text: "OK, Product gone", type: "success" } });
-  });
-});
-
-app.put("/admin/products/:id", (req, res) => {
-  const sql = `
-    UPDATE products
-    SET title = ?, price = ?, last_update = ?, cats_id = ?, in_stock = ?, photo = ?
-    WHERE id = ?
-    `;
-  con.query(
-    sql,
-    [
-      req.body.title,
-      req.body.price,
-      req.body.lu,
-      req.body.cat,
-      req.body.in_stock,
-      req.body.photo,
-      req.params.id,
-    ],
-    (err, result) => {
-      if (err) throw err;
-      res.send({
-        result,
-        msg: { text: "OK, Cat updated. Now it is as new", type: "success" },
-      });
-    }
-  );
-});
-
-app.delete("/admin/photos/:id", (req, res) => {
-  const sql = `
-    UPDATE products
-    SET photo = null
-    WHERE id = ?
-    `;
-  con.query(sql, [req.params.id], (err, result) => {
-    if (err) throw err;
-    res.send({
-      result,
-      msg: { text: "OK, photo gone. Have a nice day.", type: "success" },
-    });
-  });
-});
-
-// FRONT
-
-app.get("/products", (req, res) => {
-  const sql = `
-  SELECT p.id, price, p.title, c.title AS cat, in_stock, last_update AS lu, photo
-  FROM products AS p
-  LEFT JOIN cats AS c
-  ON c.id = p.cats_id
-  ORDER BY title
-`;
-  con.query(sql, (err, result) => {
-    if (err) throw err;
-    res.send(result);
-  });
-});
-
-app.get("/cats", (req, res) => {
-  const sql = `
-  SELECT *
-  FROM cats
-  ORDER BY title
-`;
-  con.query(sql, (err, result) => {
-    if (err) throw err;
-    res.send(result);
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Bebras klauso porto Nr ${port}`);
-});
+export default App;
