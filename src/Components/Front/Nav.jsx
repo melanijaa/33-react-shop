f0ba4e6831f19d63b6a29dc1ecd89dd24d5afd4a
Melanija <melanija.sibakovska1@gmail.com>
2022-07-11 14:25:35 +0300

    nav

---
+++ src/Components/Front/Nav.jsx
@@ -0,0 +1,15 @@
+function Nav() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <nav className="nav">Welcomen</nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
